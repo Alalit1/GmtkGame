65d95166a73826256f8d2316e56bbfe4837b1633
AlaLit <alanlitv1230@gmail.com>
2026-07-23 22:26:20 +0300

    дороботка
    юю

---
+++ combat/screpts/damage.gd
@@ -0,0 +1,15 @@
+class_name Damage
+extends RefCounted
+
+var data: DamageData
+
+var source
+var target
+
+var critical := false
+
+var final_amount := 0.0
+
+var blocked := false
+
+var dodged := false
--- technical/screpts/area_interaction.gd
+++ technical/screpts/area_interaction.gd
@@ -3,19 +3,11 @@ class_name AreaInteraction
 
 var list_item := []
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_body_entered(body: Node2D) -> void:
-	list_item.append(body)
+	if body in list_item:
+		list_item.erase(body)
 
 
 func _on_body_exited(body: Node2D) -> void:
-	pass
+	list_item.erase(body)
