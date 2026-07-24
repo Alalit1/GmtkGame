e6ceb00509a088cb74981bb1bc8e050b19fb922a
@dimaprogramer <dimon19992803@gmail.com>
2026-07-24 19:11:47 +0500

    Update game.gd

--- rooms/screpts/game.gd
+++ rooms/screpts/game.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var next_room = $next_room
 @onready var current_room = $current_room
-var new_room = preload("res://rooms/battle_room.tscn")
+var new_room = preload("res://rooms/screns/battle_room.tscn")
 
 func _ready() -> void:
 	var newroom = new_room.instantiate()
@@ -10,7 +10,7 @@ func _ready() -> void:
 	newroom.position = Vector2(0,-360)
 
 func _on_exit_body_entered(body: Node2D) -> void:
-	g.room_finish += 1
+	G.room_finish += 1
 	$player.exit()
 	var newroom = new_room.instantiate()
 	next_room.call_deferred("add_child", newroom)
@@ -20,10 +20,10 @@ func _on_exit_body_entered(body: Node2D) -> void:
 	current_room.get_child(0).queue_free()
 	next_room.get_child(0).reparent(current_room)
 	$player.exit()
-	if g.room_finish % 21 == 0:
+	if G.room_finish % 21 == 0:
 		pass
-	elif g.room_finish % 20 == 0:
+	elif G.room_finish % 20 == 0:
 		pass
 	else:
-		new_room = preload("res://rooms/battle_room.tscn")
+		new_room = preload("res://rooms/screns/battle_room.tscn")
 	
