787df4c7b89dc16bc2803778574c40d83a61ac07
@dimaprogramer <dimon19992803@gmail.com>
2026-07-24 17:07:24 +0500

    Update main.gd

--- rooms/main.gd
+++ rooms/main.gd
@@ -11,16 +11,15 @@ func _ready() -> void:
 
 func _on_exit_body_entered(body: Node2D) -> void:
 	g.room_finish += 1
-	#$player.visible = false
+	$player.exit()
 	var newroom = new_room.instantiate()
 	next_room.call_deferred("add_child", newroom)
 	newroom.position = Vector2(0,-360)
-	create_tween().tween_property(current_room.get_child(0), "position", Vector2(0, 360) ,1.5)
-	await create_tween().tween_property(next_room.get_child(0), "position", Vector2(0, 0) ,1.5).finished
+	create_tween().tween_property(current_room.get_child(0), "position", Vector2(0, 360) ,0.5)
+	await create_tween().tween_property(next_room.get_child(0), "position", Vector2(0, 0) ,0.5).finished
 	current_room.get_child(0).queue_free()
 	next_room.get_child(0).reparent(current_room)
-	$player.position = Vector2(320,340)
-	$player.visible = true
+	$player.exit()
 	if g.room_finish % 21 == 0:
 		pass
 	elif g.room_finish % 20 == 0:
