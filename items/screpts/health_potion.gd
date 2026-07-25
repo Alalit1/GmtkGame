ed79fcd53ecdf72ad9429f681b6c63717bba7cfa
@dimaprogramer <dimon19992803@gmail.com>
2026-07-25 17:57:24 +0500

    Update health_potion.gd

--- items/screpts/health_potion.gd
+++ items/screpts/health_potion.gd
@@ -3,13 +3,6 @@ extends BaseItem
 
 @export var heal_amount: int = 20
 
-
-func _on_use() -> void:
-	var player = get_tree().get_first_node_in_group("player")
-
-	if player:
-		player.heal(heal_amount)
-		
 func pick_up(player) -> void:
 	
 	if player.inventory.add_item(item_data):
