dfbbba8ce1933a378423a56a930641479f7e3b40
@dimaprogramer <dimon19992803@gmail.com>
2026-07-26 02:18:51 +0500

    Update player.gd

--- entities/player/screpts/player.gd
+++ entities/player/screpts/player.gd
@@ -11,7 +11,7 @@ var inventore = []
 
 var dash_cooldown := false
 var dashing := false
-var last_direction := Vector2(1,0)
+var last_direction := Vector2(0,1)
 var attack_charging := false
 var charge := 1.0
 var combo := 9.0
@@ -23,27 +23,35 @@ var inf_stamina = false
 
 @onready var inf_stamina_timer = $inf_stamina_timer
 @onready var attack_area = $attack_area
+@onready var player_animator = $AnimationPlayer
+@onready var player_sprite = $AnimatedSprite2D
+@onready var player_sounds = $AudioStreamPlayer2D
 
 func _physics_process(delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
 	var direction = global_position.angle_to_point(mouse_pos)
 	$attack_area.global_rotation = direction
 	if not dashing:
-		move()
+		velocity = Vector2.ZERO
 		if Input.is_action_pressed("LKM"):
 			if can_charge:
 				if inf_stamina:
 					charge += 0.0167
 					stamina -= 0.0167
+					_animation_update(direction)
 				elif stamina >= 5:
 					charge += 0.0167
 					stamina -= 0.0167
-		if Input.is_action_just_pressed("LKM"):
+					_animation_update(direction)
+		elif Input.is_action_just_pressed("LKM"):
 			attack_charging = true
-		if Input.is_action_just_released("LKM"):
-			if stamina >= 5:
-				attack()
-			attack_charging = false
+		elif Input.is_action_just_released("LKM"):
+			if inf_stamina:
+				attack(direction)
+			elif stamina >= 5:
+				attack(direction)
+		else:
+			move()
 	if Input.is_action_just_pressed("space"):
 		if can_dash == true:
 			if dash_cooldown == false:
@@ -60,10 +68,9 @@ func move():
 		Input.get_action_strength("d") - Input.get_action_strength("a"),
 		Input.get_action_strength("s") - Input.get_action_strength("w")
 	)
-	if direction != Vector2.ZERO:
-		last_direction = direction
 	direction = direction.normalized()
 	velocity = direction * speed
+	_animation_update(direction)
 
 func dash():
 	var direction = Vector2(
@@ -79,7 +86,7 @@ func dash():
 	dashing = false
 	dash_cooldown = false
 
-func attack():
+func attack(direction):
 	var enemys = $attack_area.get_overlapping_bodies()
 	if enemys.size() > 0:
 		combo += 1
@@ -88,6 +95,18 @@ func attack():
 		enemy.take_damage(final_damage)
 	stamina -= 5
 	charge = 1
+	if direction >= -PI/4 and direction < PI / 4:
+		player_sprite.play("attack_left")
+		player_sprite.flip_h = true
+	elif direction >= 3 * PI/4 or direction <= -3 * PI / 4:
+		player_sprite.play("attack_left")
+		player_sprite.flip_h = false
+	elif direction >= -3 * PI/4 and direction < -PI / 4:
+		player_sprite.play("attack_up")
+		player_sprite.flip_h = false
+	elif direction >= PI/4 and direction < 3 * PI / 4:
+		player_sprite.play("attack_down")
+		player_sprite.flip_h = false
 
 func _on_combo_timeout() -> void:
 	combo = 9
@@ -160,4 +179,51 @@ func fireball_cast():
 	pass
 
 func alohomora_cast():
-	pass
+	G.emit_signal("enemys_end")
+
+func _animation_update(direction):
+	if typeof(direction) == TYPE_FLOAT:
+		if direction >= -PI/4 and direction < PI / 4:
+			player_sprite.play("charge_left")
+			player_sprite.flip_h = true
+			last_direction = Vector2.RIGHT
+		elif direction >= 3 * PI/4 or direction <= -3 * PI / 4:
+			player_sprite.play("charge_left")
+			player_sprite.flip_h = false
+			last_direction = Vector2.LEFT
+		elif direction >= -3 * PI/4 and direction < -PI / 4:
+			player_sprite.play("charge_up")
+			player_sprite.flip_h = false
+			last_direction = Vector2.UP
+		elif direction >= PI/4 and direction < 3 * PI / 4:
+			player_sprite.play("charge_down")
+			player_sprite.flip_h = false
+			last_direction = Vector2.DOWN
+	else:
+		if direction != Vector2.ZERO:
+			last_direction = direction
+			if last_direction.x > 0:
+				player_sprite.flip_h = true
+				player_sprite.play("walk_left")
+			elif last_direction.x < 0:
+				player_sprite.flip_h = false
+				player_sprite.play("walk_left")
+			elif last_direction.y < 0:
+				player_sprite.flip_h = false
+				player_sprite.play("walk_up")
+			elif last_direction.y > 0:
+				player_sprite.flip_h = false
+				player_sprite.play("walk_down")
+		elif direction == Vector2.ZERO:
+			if last_direction.x > 0:
+				player_sprite.flip_h = true
+				player_sprite.play("idle_left")
+			elif last_direction.x < 0:
+				player_sprite.flip_h = false
+				player_sprite.play("idle_left")
+			elif last_direction.y < 0:
+				player_sprite.flip_h = false
+				player_sprite.play("idle_up")
+			elif last_direction.y > 0:
+				player_sprite.flip_h = false
+				player_sprite.play("idle_down")
