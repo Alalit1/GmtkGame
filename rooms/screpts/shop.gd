a2b414d4e008b340cd9d43c7bd21efafd9341565
@dimaprogramer <dimon19992803@gmail.com>
2026-07-25 15:53:35 +0500

    Create shop.gd

---
+++ rooms/screpts/shop.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var loot = [preload("res://Items/screens/health_potion.tscn"), preload("res://Items/screens/scroll_fire_boll.tscn")]
+var rng := RandomNumberGenerator.new()
+
+func _ready() -> void:
+	rng.randomize()
+	var spawn_loot = loot.pick_random()
+	var object = spawn_loot.instantiate()
+	object.position.x = rng.randi_range(13, 18) * 20 + 10
+	object.position.y = rng.randi_range(6, 11) * 20 + 10
+	add_child(object)
