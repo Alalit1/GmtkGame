f169ab1b5b8625265293745b17c9180ca28a793d
@dimaprogramer <dimon19992803@gmail.com>
2026-07-25 17:57:38 +0500

    Update spawner.gd

--- technical/screpts/spawner.gd
+++ technical/screpts/spawner.gd
@@ -2,13 +2,31 @@ extends Node2D
 
 var enemys_count = ceili(float(G.room_finish) / 2)
 var spawn_data = {}
+var enemys_array = [preload("res://entities/enemy/screns/goblin.tscn")]
 
 func _ready() -> void:
-	pass # Replace with function body.
+	G.connect("enemy_dead", Callable(self, "spawn"))
+	G.connect("start_spawn", Callable(self, "first_spawn"))
 
 func preparation(pos):
 	spawn_data[pos] = enemys_count
+	if spawn_data.size() == 4:
+		first_spawn()
 
 func spawn(pos):
-	spawn_data[pos] -= 1
-	
+	if spawn_data[pos] > 0:
+		var enemy_load = enemys_array.pick_random()
+		var enemy = enemy_load.instantiate()
+		get_parent().add_child(enemy)
+		enemy.position = pos
+		spawn_data[pos] -= 1
+
+func first_spawn():
+	print(spawn_data)
+	for i in spawn_data:
+		var enemy_load = enemys_array.pick_random()
+		var enemy = enemy_load.instantiate()
+		get_parent().add_child(enemy)
+		enemy.position = i
+		spawn_data[i] -= 1
+	print(spawn_data)
