68904114a30bb8fdffa4836a1f96334b301bab26
@dimaprogramer <dimon19992803@gmail.com>
2026-07-26 20:30:44 +0500

    Update shop.gd

--- rooms/screpts/shop.gd
+++ rooms/screpts/shop.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var loot = [preload("res://Items/screens/health_potion.tscn"), preload("res://Items/screens/scroll_fire_boll.tscn")]
+var loot = [preload("res://Items/screens/health_potion.tscn"), preload("res://Items/screens/scroll_fire_ball.tscn")]
 var rng := RandomNumberGenerator.new()
 
 func _ready() -> void:
