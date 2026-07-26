6b6571ee0ecfe0eb8f060f5fa6346c1294dd09cb
@dimaprogramer <dimon19992803@gmail.com>
2026-07-26 21:34:04 +0500

    Update skeletron.gd

--- entities/enemy/screpts/skeletron.gd
+++ entities/enemy/screpts/skeletron.gd
@@ -3,6 +3,7 @@ extends BaseEnemy
 @export var damage_data : DamageData 
 @export var damage_area: PackedScene
 @onready var timer: Timer = $Timer
+@onready var sprite = $AnimatedSprite2D
 var health = 10
 var amount_attack = 0
 var bullet: Node
@@ -29,12 +30,25 @@ func _physics_process(_delta):
 	#var target_point : Vector2= vision.target.global_position + vision.target.global_position.direction_to(global_position) * 15
 	var attack_position = global_position + directione * 25
 	#print(target_point,vision.target.global_position.direction_to(global_position) * 40)
-	if !is_instance_valid(bullet):
-			attack(damage_data, directione,attack_position)
+	#if !is_instance_valid(bullet):
+			#attack(damage_data, directione,attack_position)
 		
 	var direction = global_position.direction_to(vision.target.global_position)
 	velocity = direction * enemy_data.speed
 	
+	direction = global_position.angle_to_point(vision.target.global_position)
+	if direction >= -PI/4 and direction < PI / 4:
+		sprite.play("walk_left")
+		sprite.flip_h = true
+	elif direction >= 3 * PI/4 or direction <= -3 * PI / 4:
+		sprite.play("walk_left")
+		sprite.flip_h = false
+	elif direction >= -3 * PI/4 and direction < -PI / 4:
+		sprite.play("walk_up")
+		sprite.flip_h = false
+	elif direction >= PI/4 and direction < 3 * PI / 4:
+		sprite.play("walk_down")
+		sprite.flip_h = false
 	move_and_slide()
 
 func attack(damage_data:DamageData, direction, pos: Vector2):
@@ -63,6 +77,7 @@ func take_damage(amout):
 	health = -amout
 	if health <= 0 :
 		self.queue_free()
+		G.emit_signal("enemy_dead", spawn_position)
 
 func _on_timer_timeout() -> void:
 	if is_instance_valid(bullet):
