5e0fa02a2adb67366b2538660b55d5a070db3b03
@dimaprogramer <dimon19992803@gmail.com>
2026-07-26 15:56:24 +0500

    Update spawner.gd

--- technical/screpts/spawner.gd
+++ technical/screpts/spawner.gd
@@ -7,11 +7,19 @@ var enemys_array = [preload("res://entities/enemy/screns/skeletron.tscn")]
 func _ready() -> void:
 	G.connect("enemy_dead", Callable(self, "spawn"))
 	G.connect("start_spawn", Callable(self, "first_spawn"))
+	G.connect("boss_cast", Callable(self, "spawn_all"))
 
 func preparation(pos):
 	spawn_data[pos] = enemys_count
-	if spawn_data.size() == 4:
+	if spawn_data.size() == 4 and not G.boss:
 		first_spawn()
+
+func spawn_all():
+	for i in spawn_data:
+		var enemy_load = enemys_array.pick_random()
+		var enemy = enemy_load.instantiate()
+		get_parent().add_child(enemy)
+		enemy.position = i
 
 func spawn(pos):
 	if spawn_data[pos] > 0:
